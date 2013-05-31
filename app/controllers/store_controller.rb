369e464221f9c972c11376d5cb7dab8d80838140
Austin Czarnecki <austin.czarnecki@yale.edu>
2013-05-31 15:45:15 +0000

    Added session counter to track visits to catalog

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	increment_counter
+  end
+
+  def increment_counter
+  	if session[:counter] ||= 0
+  	end
+  	session[:counter] += 1
   end
 end
