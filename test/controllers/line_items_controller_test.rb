087fe7e14671022d74ba654c16e1a8a09d49ff2d
Austin Czarnecki <austin.czarnecki@yale.edu>
2013-05-31 17:36:13 +0000

    added product price to line items table, changed unit test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -18,7 +18,8 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, product_id: products(:ruby).id
+      @line_item = { product_id: products(:ruby).id, price: products(:ruby).price }
+      post :create, @line_item
     end
 
     assert_redirected_to cart_path(assigns(:line_item).cart)
